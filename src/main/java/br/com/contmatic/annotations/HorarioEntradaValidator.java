ba80167cd5b7517184abf1b7b30c2d4dba938d51
Vitor <vitor.nunes@contmatic.com.br>
2020-07-21 14:51:09 -0300

    commit guava

--- src/main/java/br/com/contmatic/annotations/HorarioEntradaValidator.java
+++ src/main/java/br/com/contmatic/annotations/HorarioEntradaValidator.java
@@ -5,6 +5,8 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.joda.time.LocalTime;
 
+import com.google.common.base.Optional;
+
 // TODO: Auto-generated Javadoc
 /**
  * The Class HorarioEntradaValidator.
@@ -19,10 +21,15 @@ public class HorarioEntradaValidator implements ConstraintValidator<HorarioEntra
      * @return true, if is valid
      */
     public boolean isValid(LocalTime horario, ConstraintValidatorContext cvc) {
-        if (horario != null && (horario.isBefore(new LocalTime(07, 00, 00)) || horario.isAfter(new LocalTime(18, 00, 00)))) {
-            return false;
-        }
-        return true;
+        Optional<LocalTime> optHorario = Optional.fromNullable(horario);
+        return !(optHorario.or(LocalTime.now()).isBefore(new LocalTime(07, 00, 00)) || optHorario.or(LocalTime.now()).isAfter(new LocalTime(18, 00, 00)));
     }
+
+//    public boolean isValid(LocalTime horario, ConstraintValidatorContext cvc) {
+//        if (horario != null && (horario.isBefore(new LocalTime(07, 00, 00)) || horario.isAfter(new LocalTime(18, 00, 00)))) {
+//            return false;
+//        }
+//        return true;
+//    }
 }
 
--- src/main/java/br/com/contmatic/annotations/HorarioRetornoAlmocoValidator.java
+++ src/main/java/br/com/contmatic/annotations/HorarioRetornoAlmocoValidator.java
@@ -5,6 +5,8 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.joda.time.LocalTime;
 
+import com.google.common.base.Optional;
+
 // TODO: Auto-generated Javadoc
 /**
  * The Class HorarioRetornoAlmocoValidator.
@@ -19,10 +21,14 @@ public class HorarioRetornoAlmocoValidator implements ConstraintValidator<Horari
      * @return true, if is valid
      */
     public boolean isValid(LocalTime horario, ConstraintValidatorContext cvc) {
-        if (horario != null && (horario.isBefore(new LocalTime(12, 00, 00)) || horario.isAfter(new LocalTime(15, 00, 00)))) {
-            return false;
-        }
-        return true;
+        Optional<LocalTime> optHorario = Optional.fromNullable(horario);
+        return !(optHorario.or(LocalTime.now()).isBefore(new LocalTime(12, 00, 00)) || optHorario.or(LocalTime.now()).isAfter(new LocalTime(15, 00, 00)));
     }
+    // public boolean isValid(LocalTime horario, ConstraintValidatorContext cvc) {
+    // if (horario != null && (horario.isBefore(new LocalTime(12, 00, 00)) || horario.isAfter(new LocalTime(15, 00, 00)))) {
+    // return false;
+    // }
+    // return true;
+    // }
 
 }
